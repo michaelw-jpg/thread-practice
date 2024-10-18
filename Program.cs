12a7dc9152ce2cfb33bb02ca80624ff32a6bbae9
michael.wortel@chasacademy.se <michael.wortel@chasacademy.se>
2024-10-18 09:36:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace threadsp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Thread Thread1 = new Thread(new ThreadStart(Counter));
+            Thread Thread2 = new Thread(new ThreadStart(CounterTwo));
+            Thread1.Start();
+            Thread2.Start();
+
+            Thread1.Join();
+            Thread2.Join();
+
+            Console.WriteLine("nu är båda trådar klara");
+            
+        }
+        static void CounterTwo()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(200);
+            }
+        }
+        
+        static void Counter()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
